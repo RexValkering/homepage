11bafb102b89016869e9316823f017a45bcf0b18
Rex Valkering <rexvalkering@gmail.com>
2019-11-14 18:12:04 +0100

    Changed degree.

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@
         <div class="col col-md-3 col-sm-6 col-12">
           <img style="max-width: 100%" src="https://avatars0.githubusercontent.com/u/1303929?s=460&v=4">
           <h3 class="mt-4 mb-4"><strong>Rex Valkering</strong></h3>
-          <p>I am a full-stack developer and security analyst with a bachelor's degree in Computer Science and roughly 9 years of experience. I specialize in creation of administration software and security.</p>
+          <p>I am a full-stack developer and security analyst with a master's degree in Computational Science and roughly 9 years of experience. I specialize in creation of administration software and security.</p>
           <p>In my free time I spend a lot of time on big and small IT projects. On the right you will find a selection of my voluntary project portfolio.</p>
           <h4 class="mt-4">Contact info</h4>
           <p><i>myfullname at gmail dot com</i></p>
