5b95f2046afadf53cf0c65d291fb9cf6e003717d
Rex Valkering <rexvalkering@gmail.com>
2021-04-02 15:37:02 +0200

    Upgraded mdbvue version

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import 'bootstrap-css-only/css/bootstrap.min.css';
-import 'mdbvue/build/css/mdb.css';
+import 'bootstrap-css-only/css/bootstrap.min.css'
+import 'mdbvue/lib/css/mdb.min.css'
+import '@fortawesome/fontawesome-free/css/all.min.css'
+
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 import App from './App';
